485159362707a69f39565dc398ac97747a66026c
unknown <ndoannguyen@NCEL31631.iis.amadeus.net>
2018-01-26 14:10:36 +0100

    Amphi TD3 not completed

--- Lesson3/TD/DataProcessingVNExpress.py
+++ Lesson3/TD/DataProcessingVNExpress.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import time
 from multiprocessing import Pool
 
-DATA_DIR = "../../FrequencyData/"
+DATA_DIR = "./"
 TMP_DIR = "tmp"
 
 CAT_ID = {          "THOI_SU":          1001005, 
@@ -31,8 +31,6 @@ CAT_ID = {          "THOI_SU":          1001005,
         }
 
 URL_MODEL = {}
-FROMDATE = 1500069600
-TODATE = 1500153144
 BASE_URL = "http://vnexpress.net/category/day/"
 VIDEO_PREFIX = "video.vnexpress.net"
 
@@ -120,6 +118,7 @@ def store_articles_content(titlefile, datafile):
 
 def partially_store_articles_content(title):
     try:
+        print("Downloading %s", title)
         title_news, short_intro, contents = get_article_content(title)
         res = ""
         if title_news != None:
@@ -155,17 +154,11 @@ def clean_content(string):
         string = string[:-1]
     return string
 
-#prepare_stockage()
+store_article_url("01/01/18", "31/01/18", "News_012018.txt", "THOI_SU")
+store_articles_content("News_012018.txt", "News_012018_content.txt")
+
 
 """
-store_article_url(1500069600, 1500153144, "articles_1507.txt")
-store_articles_content("articles_1507.txt" , "data_1507.txt")
-store_article_url(1499983200, 1500066744, "articles_1407.txt")
-store_articles_content("articles_1407.txt" , "data_1407.txt")
-"""
-
-#store_article_url("01/01/17", "31/01/17", "SportTitles_Jan2017.txt", "Thể thao")
-#store_articles_content("SportTitles_Jan2017.txt", "Sport_Jan2017.txt")
 begin = ["01/01/17", "01/02/17", "01/03/17", "01/04/17", "01/05/17", "01/06/17", "01/07/17", "01/08/17", "01/09/17", "01/10/17", "01/11/17", "01/12/17"]
 end = ["31/01/17", "28/02/17", "31/03/17", "30/04/17", "31/05/17", "30/06/17", "31/07/17", "31/08/17", "30/09/17", "31/10/17", "30/11/17", "31/12/17"]
 CatE = "KHOA_HOC"
@@ -173,6 +166,8 @@ cate = "Science"
 mth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 if __name__ == '__main__':
+    #prepare_stockage()
+
     for i in range(0, 12):
         t1 = time.time()
         store_article_url(begin[i], end[i], cate + "Titles_" + mth[i] + "2017.txt", CatE)
@@ -180,3 +175,4 @@ if __name__ == '__main__':
         print("Step 1 took " + str(t2 - t1) + " seconds.")
         store_articles_content(cate + "Titles_" + mth[i] + "2017.txt", cate + "_" + mth[i] + "2017.txt")
         print("Step 2 took " + str(time.time() - t2) + " seconds.")
+"""
--- Lesson3/TD/Maths_Solution.py
+++ Lesson3/TD/Maths_Solution.py
@@ -8,6 +8,7 @@ Created on Sat Jan 20 21:47:00 2018
 import numpy as np
 import scipy as sp
 from scipy import optimize
+import matplotlib.pyplot as plt
 
 A = np.array([ [ 0, 1,  0,  0], 
                [ 1, 2, -2,  0],
@@ -91,8 +92,11 @@ def ex7():
     print( np.linalg.solve((A + np.eye(4)), v) )
 
 f = lambda x: 4 * np.sin(x) - np.cos(x) + 2 * np.exp(-3*x**2) + 1./x 
-F = lambda x: float((x[0] - x[1])**2) / (x[0]**2 + x[1]**2)
+F = lambda x, y: float((x - y)**2) / (x**2 + y**2)
+
+F_rewrite = lambda x: float((x[0] - x[1])**2) / (x[0]**2 + x[1]**2)
 #F = lambda x: x[0] + x[1]**2
+#F = lambda x, y: 1
 
 # Exercise 8
 def ex8():
@@ -103,9 +107,31 @@ def ex8():
 
 #Exercise 9
 def ex9():
-    print( optimize.approx_fprime((1, -2), F, 1e-8))
+    print( optimize.approx_fprime((1, -2), F_rewrite, 1e-8))
 
+def ex10():
+    print( sp.integrate.quad(f, 1, 2) )
+    print( sp.integrate.dblquad(F, 0, 2, lambda x: max(0, np.sqrt(1 - x**2)), lambda x: np.sqrt(4 - x**2) ))
 
+#Exercise 11
+def ex11():
+    X = np.linspace(-10, 10, 101)
+    Y = map(f, X)
+    plt.plot(X, Y, linestyle="-")
+    plt.title("y = f(x)")
+    plt.legend("y = f(x)")
+    plt.show()
+    for x0_estimated in [-9, -6, -3, 3, 6.48, 10]:
+        print ( optimize.root(f, x0 = x0_estimated, method='broyden2').x )
+
+#Exercise 12
+def ex12():
+    print("Local minima: ")
+    for x0_estimated in [-8, -1, 0.5, 5]:
+        print ( optimize.minimize(f, x0 = x0_estimated, method='Nelder-Mead' ).x ) 
+    print("Local maxima: ")
+    for x0_estimated in [-5, -0.5, 1, 8]:
+        print ( optimize.minimize(lambda x: -f(x), x0 = x0_estimated, method='Nelder-Mead' ).x ) 
     
 if __name__ == "__main__":
-    ex9()
+    ex12()
