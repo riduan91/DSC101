a9ece95e30988a057068ff89e57bee666fa56dfd
riduan91 <ndoannguyen@NCEL31631.iis.amadeus.net>
2018-01-27 22:12:43 +0100

    Amphi 3completed

---
+++ Lesson3/Amphi/Hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 22:07:45 2018
+
+@author: ndoannguyen
+"""
+
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    print("Hello %s!" % user)
