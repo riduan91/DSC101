b88498594ec26b95acec3e8eb735be9bbe24811e
ndoannguyen <riduan91@gmail.com>
2018-02-02 18:45:01 +0100

    TD3

--- Lesson3/TD/TestVNExpressPreProcessing.py
+++ Lesson3/TD/TestVNExpressPreProcessing.py
@@ -6,7 +6,24 @@ Created on Fri Feb 02 15:44:24 2018
 """
 
 import sys, os, shutil
-from VNExpressPreProcessing import prepareDataFolder
+from VNExpressPreProcessing import prepareDataFolder, dayToTimestamp, downloadFirstTitlePage
+
+#CATEGORIES
+THOI_SU = 1001005 
+GIA_DINH = 1002966 
+SUC_KHOE = 1003750 
+THE_GIOI = 1001002 
+KINH_DOANH = 1003159 
+GIAI_TRI = 1002691 
+THE_THAO = 1002565 
+PHAP_LUAT = 1001007 
+GIAO_DUC = 1003497 
+DU_LICH = 1003231 
+KHOA_HOC = 1001009 
+SO_HOA = 1002592 
+XE = 1001006 
+CONG_DONG = 1001012 
+TAM_SU = 1001014
 
 def test0():
     print("Test 0 OK.")
@@ -38,8 +55,29 @@ def test2():
             os.rmdir("clrscr")
             print("Test 2 OK.")
     
+def test3():
+    s = dayToTimestamp("21/01/2018")
+    if not isinstance(s, int) and not isinstance(s, long):
+        print("Please convert your result to type int.")
+        print("Test 3 not OK")
+    else:
+        if s != 1516489200:
+            print("Test 3 not OK. 21/01/2018 should be converted to 1516489200.")
+        else:
+            print("Test 3 OK.")
 
-Tests = [test0, test1, test2]
+def test4():
+    s = downloadFirstTitlePage(THOI_SU, "01/01/2018", "02/01/2018")
+    if s.find("Tài xế trả tiền xu") <= 0:
+        print("Page not successfully downloaded. Test 4 not OK")
+    else:
+        print("Test 4 OK")
+
+def test5():
+    pass
+
+
+Tests = [test0, test1, test2, test3, test4, test5]
 
 #MAIN FUNCTION
 if __name__ == '__main__':
--- Lesson3/TD/VNExpressPreProcessing.py
+++ Lesson3/TD/VNExpressPreProcessing.py
@@ -5,10 +5,29 @@ Created on Fri Feb 02 15:42:25 2018
 @author: ndoannguyen
 """
 
-import os
+import os, time, urllib
 
 # Exercise 1
 from bs4 import BeautifulSoup
+
+#CATEGORIES
+THOI_SU = 1001005 
+GIA_DINH = 1002966 
+SUC_KHOE = 1003750 
+THE_GIOI = 1001002 
+KINH_DOANH = 1003159 
+GIAI_TRI = 1002691 
+THE_THAO = 1002565 
+PHAP_LUAT = 1001007 
+GIAO_DUC = 1003497 
+DU_LICH = 1003231 
+KHOA_HOC = 1001009 
+SO_HOA = 1002592 
+XE = 1001006 
+CONG_DONG = 1001012 
+TAM_SU = 1001014
+
+BASE_URL = "http://vnexpress.net/category/day/"
 
 def prepareDataFolder(data_folder):
     # Exercise 2
@@ -19,3 +38,17 @@ def prepareDataFolder(data_folder):
         print("Folder '" + data_folder + "' already existed." )
     else:
         os.mkdir(data_folder)
+
+def dayToTimestamp(day):
+    # Exercise 3
+    # TODO
+    return int(time.mktime(time.strptime(day, "%d/%m/%Y")))
+
+def downloadFirstTitlePage(category, fromdate, todate):
+    # Exercise 4
+    # TODO
+    url = "%s?cateid=%d&fromdate=%d&todate=%d&allcate=%d|%d|" \
+        % (BASE_URL, category, dayToTimestamp(fromdate), dayToTimestamp(todate), category, category)
+    return urllib.urlopen(url).read()
+
+def 
